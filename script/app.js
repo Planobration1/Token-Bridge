ba60075e07e85cc7b26c2c16dee0a3e7b336e166
Steve Tomi <89354038+SteffQing@users.noreply.github.com>
2023-08-28 16:19:35 +0100

    handle node server response crudely

--- script/app.js
+++ script/app.js
@@ -11,6 +11,47 @@ async function getBlockTimestamp() {
   const _bscblock = await provider.getBlockNumber();
   return [_bscblock, _timestamp];
 }
+
+async function bscFallback(bscBlock, filter) {
+  const { provider, bridgeContract: BSC } = bscContract();
+  const block = await provider.getBlockNumber();
+  const events = await BSC.queryFilter(filter, bscBlock, block);
+  bscBlock = block;
+  for (let event of events) {
+    let { transactionHash, args } = event;
+    let exists = await redisClient("get", transactionHash);
+    if (!exists) {
+      await redisClient("set", transactionHash);
+      const [from, to, value] = args;
+      console.log(from, to, value, "BSC Handler fallback");
+      await BscToTrc(from, to, value.toString());
+    }
+  }
+  return block;
+}
+
+async function trcLoop(trcTimestamp, contract) {
+  const blockHex = await tronWeb.trx.getCurrentBlock();
+  const _timestamp = blockHex.block_header.raw_data.timestamp;
+  const events = await tronWeb.getEventResult(contract, {
+    eventName: "Deposit",
+    onlyConfirmed: true,
+    sinceTimestamp: trcTimestamp,
+    sort: "block_timestamp",
+  });
+  for (let event of events) {
+    const { result, transaction } = event;
+    let exists = await redisClient("get", transaction);
+    if (!exists) {
+      await redisClient("set", transaction);
+      const { from, to, value } = result;
+      console.log(from, to, value, "Tron Handler");
+      await TrcToBsc(from, to, value.toString());
+    }
+  }
+  return _timestamp;
+}
+
 async function main() {
   console.log("Script start");
   const sevenMinutes = 420000;
@@ -18,8 +59,8 @@ async function main() {
   /// @dev get latest block number and timestamp for both chains
   let [bscBlock, trcTimestamp] = await getBlockTimestamp();
 
-  /// @dev bsc bridge handler
-  const { bridgeContract: BSC, provider } = bscContract();
+  /// @dev bsc bridge handler websocket
+  const { bridgeContract: BSC } = bscContract();
   const filter = BSC.filters["Deposit"]();
   BSC.on(filter, async (event) => {
     try {
@@ -37,25 +78,7 @@ async function main() {
   const contract = config.trx.bridge;
   setInterval(async () => {
     try {
-      const blockHex = await tronWeb.trx.getCurrentBlock();
-      const _timestamp = blockHex.block_header.raw_data.timestamp;
-      const events = await tronWeb.getEventResult(contract, {
-        eventName: "Deposit",
-        onlyConfirmed: true,
-        sinceTimestamp: trcTimestamp,
-        sort: "block_timestamp",
-      });
-      trcTimestamp = _timestamp;
-      for (let event of events) {
-        const { result, transaction } = event;
-        let exists = await redisClient("get", transaction);
-        if (!exists) {
-          await redisClient("set", transaction);
-          const { from, to, value } = result;
-          console.log(from, to, value, "Tron Handler");
-          await TrcToBsc(from, to, value.toString());
-        }
-      }
+      trcTimestamp = await trcLoop(trcTimestamp, contract);
     } catch (error) {
       console.log(
         errorHandler("TRC", error, "app.js", "tronWeb.getEventResult")
@@ -66,22 +89,14 @@ async function main() {
   /// @dev fallback for bsc bridge handler
   setInterval(async () => {
     try {
-      const block = await provider.getBlockNumber();
-      const events = await BSC.queryFilter(filter, bscBlock, block);
-      bscBlock = block;
-      for (let event of events) {
-        let { transactionHash, args } = event;
-        let exists = await redisClient("get", transactionHash);
-        if (!exists) {
-          await redisClient("set", transactionHash);
-          const [from, to, value] = args;
-          console.log(from, to, value, "BSC Handler fallback");
-          await BscToTrc(from, to, value.toString());
-        }
-      }
+      bscBlock = await bscFallback(bscBlock, filter);
     } catch (error) {
-      console.log(errorHandler("BSC", error, "app.js", "BSC.queryFilter"));
+      if (error.message.includes("server response 503 Service Unavailable")) {
+        console.log("BSC server response 503 Service Unavailable");
+        bscBlock = await bscFallback(bscBlock, filter);
+      } else
+        console.log(errorHandler("BSC", error, "app.js", "BSC.queryFilter"));
     }
   }, sevenMinutes);
 }
-main().catch((e) => console.log(e));
+main().catch((e) => console.log(errorHandler("Bridge", e, "app.js", "main()")));
