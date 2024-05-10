4f12e2c6c1832304a10787f2b5728ef9c766f809
Steve Tomi <steveola23@gmail.com>
2024-05-10 09:14:47 +0100

    Switch to vercel KV store

--- script/redis.js
+++ script/redis.js
@@ -1,25 +1,16 @@
-const { createClient } = require("redis");
+const { kv } = require("@vercel/kv");
 const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 
 async function redisClient(action, hash) {
-  const client = createClient({
-    url: process.env.REDIS_URL,
-  });
-
   let result = false;
 
-  await client.connect();
-
   if (action === "set") {
-    await client.sAdd("transactions", hash);
+    await kv.sadd("transactions", hash);
   }
   if (action === "get") {
-    result = await client.sIsMember("transactions", hash);
+    result = await kv.sismember("transactions", hash);
   }
-
-  await client.quit();
-
   return result;
 }
 
